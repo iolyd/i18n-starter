3ab873df3a9af941c961743363205e02e66f5473
Emmanuel <emmbema@gmail.com>
2023-11-24 16:45:39 -0500

    Add simple mount count component

---
+++ src/lib/components/MountCount.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const counts = writable<Record<string, number>>({});
+</script>
+
+<script lang="ts">
+	export let key: string;
+
+	onMount(() => {
+		counts.update((v) => {
+			return { ...v, [key]: (v[key] ?? 0) + 1 };
+		});
+	});
+</script>
+
+{$counts[key]}
