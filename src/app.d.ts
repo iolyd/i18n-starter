aa05bfb25ddc9c3464d0e904c7513a6e0f343d7f
Emmanuel <emmbema@gmail.com>
2023-11-24 14:42:09 -0500

    Add lang type declaration for event locals and page data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { LanguageTag } from '$lib/i18n/types';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			lang: LanguageTag;
+		}
+		interface PageData {
+			lang: LanguageTag;
+		}
 		// interface Platform {}
 	}
 }
